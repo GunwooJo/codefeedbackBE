9752ce2874f59748e552a38e9fc0f74b3619f93f
choihojean <145415865+choihojean@users.noreply.github.com>
2024-05-30 15:55:52 +0900

    feat: UserUpdate

--- src/main/java/cloudweb/codefeedbackBE/service/UserService.java
+++ src/main/java/cloudweb/codefeedbackBE/service/UserService.java
@@ -1,6 +1,7 @@
 package cloudweb.codefeedbackBE.service;
 
 import cloudweb.codefeedbackBE.dto.LoginUserDTO;
+import cloudweb.codefeedbackBE.dto.UpdateUserDTO;
 import cloudweb.codefeedbackBE.dto.UserDTO;
 import cloudweb.codefeedbackBE.entity.User;
 import cloudweb.codefeedbackBE.repository.UserRepository;
@@ -8,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -37,4 +39,15 @@ public class UserService {
                 .nickname(loginedUser.getNickname())
                 .build();
     }
+
+    public User updateUser(String userId, UpdateUserDTO updateUserDTO) {
+        Optional<User> userOptional = userRepository.findById(userId);
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+            user.setNickname(updateUserDTO.getNickname());
+            user.setPassword(updateUserDTO.getPassword());
+            return user;
+        } else {
+            throw new RuntimeException("User not found");
+        }
 }
