817a0b950fd1a83897ddd5daef3a057f26dfdd0c
GunwooJo <kanggi1997@naver.com>
2024-05-30 13:52:28 +0900

    refactor: jpa에 맞게 User객체 수정.

--- src/main/java/cloudweb/codefeedbackBE/entity/User.java
+++ src/main/java/cloudweb/codefeedbackBE/entity/User.java
@@ -1,16 +1,25 @@
 package cloudweb.codefeedbackBE.entity;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 @Getter @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
+@Entity
 public class User {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(length = 30, nullable = false)
     private String email;
+
+    @Column(length = 60, nullable = false)
     private String password;
+
+    @Column(length = 20, nullable = false)
     private String nickname;
 
 }
