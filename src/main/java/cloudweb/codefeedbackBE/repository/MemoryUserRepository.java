df993e218f248dee8b62a29b8c8d8113cdbdfcfa
GunwooJo <kanggi1997@naver.com>
2024-05-30 14:11:56 +0900

    MemoryUserRepository 주석처리

--- src/main/java/cloudweb/codefeedbackBE/repository/MemoryUserRepository.java
+++ src/main/java/cloudweb/codefeedbackBE/repository/MemoryUserRepository.java
@@ -1,33 +1,33 @@
-package cloudweb.codefeedbackBE.repository;
-
-import cloudweb.codefeedbackBE.entity.User;
-import org.springframework.stereotype.Repository;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.NoSuchElementException;
-
-@Repository
-public class MemoryUserRepository implements UserRepository{
-
-    private Long sequence = 0L;
-    private final Map<Long, User> db = new HashMap<>();
-
-    @Override
-    public Long save(User user) {
-        user.setId(sequence++);
-        db.put(user.getId(), user);
-        return user.getId();
-    }
-
-    @Override
-    public void deleteUserByEmail(String email) {
-
-        User foundUser = db.values().stream()
-                .filter(user -> user.getEmail().equals(email))
-                .findAny()
-                .orElseThrow(() -> new NoSuchElementException("해당 email을 가진 user가 없습니다."));
-
-        db.remove(foundUser.getId());
-    }
-}
+//package cloudweb.codefeedbackBE.repository;
+//
+//import cloudweb.codefeedbackBE.entity.User;
+//import org.springframework.stereotype.Repository;
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//import java.util.NoSuchElementException;
+//
+//@Repository
+//public class MemoryUserRepository implements UserRepository{
+//
+//    private Long sequence = 0L;
+//    private final Map<Long, User> db = new HashMap<>();
+//
+//    @Override
+//    public Long save(User user) {
+//        user.setId(sequence++);
+//        db.put(user.getId(), user);
+//        return user.getId();
+//    }
+//
+//    @Override
+//    public void deleteUserByEmail(String email) {
+//
+//        User foundUser = db.values().stream()
+//                .filter(user -> user.getEmail().equals(email))
+//                .findAny()
+//                .orElseThrow(() -> new NoSuchElementException("해당 email을 가진 user가 없습니다."));
+//
+//        db.remove(foundUser.getId());
+//    }
+//}
