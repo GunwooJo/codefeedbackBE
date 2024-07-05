04d7d073d73ea95e4e686bd6aa38ca7dd5c364f0
GunwooJo <kanggi1997@naver.com>
2024-07-06 00:08:29 +0900

    fix: 허용 origin 수정

--- src/main/java/cloudweb/codefeedbackBE/config/WebConfig.java
+++ src/main/java/cloudweb/codefeedbackBE/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // 모든 경로에 대해
-                .allowedOrigins("http://localhost:3000")  // 허용할 Origin
+                .allowedOrigins("http://gunwoo.site")  // 허용할 Origin
                 .allowedMethods("GET", "POST", "PUT", "DELETE")  // 허용할 HTTP 메소드
                 .allowedHeaders("*")  // 모든 헤더 허용
                 .allowCredentials(true)  // Credential 허용
